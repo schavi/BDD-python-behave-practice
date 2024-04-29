c847c43913d1e5c2f7feeac2ce8e6d948e6fcf86
kalla <aronschn@gmail.com>
2024-04-29 19:19:08 +0200

    first commit

---
+++ features/steps/page_model.py
@@ -0,0 +1,42 @@
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from general_page import GeneralPage
+
+
+
+class Ecosia(GeneralPage):
+
+    def __init__(self, chosen_browser):
+        super().__init__(url='https://ecosia.org/',chosen_browser=chosen_browser)
+        self.wait = WebDriverWait(self.browser, 5)
+
+    
+
+    def main_search_input(self) -> WebElement:
+        return self.browser.find_element(By.XPATH, "//input[@type='search']")
+
+    def main_search_button(self) -> WebElement:
+        return self.browser.find_element(By.XPATH, "//button[@type='submit']")
+    
+
+    def result_images_tab_button(self) -> WebElement:
+        return self.browser.find_element(By.XPATH, "//div[@data-test-id='search-navigation-images']/a")
+
+    def result_news_tab_button(self) -> WebElement:
+        return self.browser.find_element(By.XPATH, "//div[@data-test-id='search-navigation-news']/a")
+    
+    def result_videos_tab_button(self) -> WebElement:
+        return self.browser.find_element(By.XPATH, "//div[@data-test-id='search-navigation-videos']/a")
+
+
+    def result_all_links(self) -> list[WebElement]:
+        return self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[@data-test-id='result-link']")))
+    
+    def result_images_articles(self) -> list[WebElement]:
+        return self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//article[@data-test-id='images-result']")))
---
+++ features/steps/steps.py
@@ -0,0 +1,34 @@
+
+import behave
+
+from time import sleep
+
+from page_model import Ecosia
+
+@given("Opening ecosia.org")
+def open_ecosia(context):
+    context.page = Ecosia(chosen_browser="firefox")
+    context.page.open()
+
+@when("Entering '{query}' into the search bar")
+def enter_search_query(context, query):
+    context.page.main_search_input().send_keys(query)
+
+@when("Clicking the search button")
+def click_search_button(context):
+    context.page.main_search_button().click()
+
+@then("The behave documentation shows up in the results")
+def check_behave_docs_in_results(context):
+    assert "https://behave.readthedocs.io/en/stable/" in [link.get_attribute("href") for link in context.page.result_all_links()]
+    context.page.close()
+
+@when("Clicking the images tab")
+def clicking_the_images_tab(context):
+    context.page.result_images_tab_button().click()
+
+@then("Images of '{query}'s are shown")
+def relevant_images_are_shown(context, query):
+    image_result_labels = [element.get_attribute("aria-label") for element in context.page.result_images_articles()]
+    assert any(query in label for label in image_result_labels)
+    context.page.close()
