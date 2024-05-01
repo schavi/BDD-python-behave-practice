154942cf88a09768f6d1122f5d34ab57938b1543
kalla <aronschn@gmail.com>
2024-05-01 15:05:23 +0200

    step names rewritten

--- features/ecosia.feature
+++ features/ecosia.feature
@@ -3,15 +3,14 @@ Feature: Searching the web
     Users should be able to retrieve results from the internet based on keywords.
     They should be able to pick between general results, images only, videos only and news.
 
+
     Scenario: General search
-        Given Opening ecosia.org
-        When Entering 'python behave' into the search bar
-        And Clicking the search button
-        Then The behave documentation shows up in the results
+        Given ecosia.org is open in 'firefox'
+        When the user enters 'python behave' into the search bar
+        Then the behave documentation shows up in the results
     
     Scenario: Image search
-        Given Opening ecosia.org
-        When Entering 'kittycat' into the search bar
-        And Clicking the search button
-        And Clicking the images tab
-        Then Images of 'kittycat's are shown
+        Given ecosia.org is open in 'firefox'
+        When the user enters 'kittycat' into the search bar
+        And the user clicks on the images tab
+        Then images of 'kittycat's are shown
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -5,29 +5,26 @@ from time import sleep
 
 from POM.page_model import Ecosia
 
-@given("Opening ecosia.org")
-def open_ecosia(context):
-    context.page = Ecosia(chosen_browser="firefox")
+@given("ecosia.org is open in '{browser}'")
+def open_ecosia(context, browser):
+    context.page = Ecosia(chosen_browser=browser)
     context.page.open()
 
-@when("Entering '{query}' into the search bar")
+@when("the user enters '{query}' into the search bar")
 def enter_search_query(context, query):
     context.page.main_search_input().send_keys(query)
-
-@when("Clicking the search button")
-def click_search_button(context):
     context.page.main_search_button().click()
 
-@then("The behave documentation shows up in the results")
+@then("the behave documentation shows up in the results")
 def check_behave_docs_in_results(context):
     assert "https://behave.readthedocs.io/en/stable/" in [link.get_attribute("href") for link in context.page.result_all_links()]
     context.page.close()
 
-@when("Clicking the images tab")
+@when("the user clicks on the images tab")
 def clicking_the_images_tab(context):
     context.page.result_images_tab_button().click()
 
-@then("Images of '{query}'s are shown")
+@then("images of '{query}'s are shown")
 def relevant_images_are_shown(context, query):
     image_result_labels = [element.get_attribute("aria-label") for element in context.page.result_images_articles()]
     assert any(query in label for label in image_result_labels)
